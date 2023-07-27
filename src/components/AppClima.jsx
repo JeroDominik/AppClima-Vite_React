cec68e666aafa9c04ded310add8209a99a060bf1
Jero Dominik <jeronimo.dominik@gmail.com>
2023-07-27 12:16:22 -0300

    Commit Creando App Clima

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,13 @@
+import Formulario from './Formulario'
+
+export default function AppClima() {
+  return (
+    <>
+        <main className='dos-columnas'>
+            <Formulario/>
+
+            
+        </main>
+    </>
+  )
+}
